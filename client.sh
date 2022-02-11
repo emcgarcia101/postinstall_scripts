6033bdb1dc4025ae20a640262b4e05957e28ba3d
Emanuel Garcia <64049524+emcgarcia101@users.noreply.github.com>
2022-02-10 22:33:46 -0300

    Update client.sh

--- client.sh
+++ client.sh
@@ -53,12 +53,19 @@ echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.
 apt-get update && apt-get install spotify-client -y
 
 ###VS CODE###
-wget -o "vscode_download.deb" https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 
-dpkg -i vscode_download.deb
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
 
 ###GOOGLE CHROME###
-wget -o "chrome_download.deb" https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
-dpkg -i chrome_download.deb
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt install google-chrome-stable -y
 
 ###DBEAVER###
 wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | apt-key add -

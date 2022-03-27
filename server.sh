65ba39f750133ff9261222986bae3dd07e2740bf
Emanuel Garcia <64049524+emcgarcia101@users.noreply.github.com>
2022-03-27 17:43:27 -0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ###BASIC SOFTWARE FOR THIS SCRIPT###
-apt-get update --allow-releaseinfo-change
-apt-get install apt-transport-https -y
-apt-get install curl -y
-apt-get install git -y
-apt-get install build-essential dkms -y
+apt update --allow-releaseinfo-change
+apt install apt-transport-https -y
+apt install curl -y
+apt install git -y
+apt install build-essential dkms -y
 
 ###ADD REPOSITORIES TO UBUNTU BASE DISTROS###
 add-apt-repository universe -y
@@ -13,28 +13,32 @@ add-apt-repository multiverse -y
 add-apt-repository restricted -y
 
 ###REPOSITORIES UPDATE AND UPGRADE THE SYSTEM###
-apt-get update
-apt-get dist-upgrade -y
+apt update
+apt dist-upgrade -y
 
 ###INSTALL PACKAGES###
-apt-get install exfat-fuse exfat-utils -y
-apt-get install rar unrar p7zip-full p7zip-rar -y
-apt-get install inetutils-traceroute -y
-apt-get install htop -y
-apt-get install nmap -y
-apt-get install tcpdump -y
-apt-get install tshark -y
-apt-get install ncdu -y
-apt-get install lm-sensors -y
-apt-get install speedtest-cli -y
-apt-get install neofetch -y
-apt-get install nload -y
-apt-get install iperf -y
-apt-get install iperf3 -y
-apt-get install openssh-server -y
+apt install exfat-fuse exfat-utils -y
+apt install rar unrar p7zip-full p7zip-rar -y
+apt install inetutils-traceroute -y
+apt install htop -y
+apt install nmap -y
+apt install tcpdump -y
+apt install tshark -y
+apt install ncdu -y
+apt install lm-sensors -y
+apt install neofetch -y
+apt install nload -y
+apt install iperf -y
+apt install iperf3 -y
+apt install openssh-server -y
+
+###SPEEDTEST###
+apt install curl -y
+curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+apt install speedtest -y
 
 ###UPDATE ALL AND FIX THINGS###
-apt-get install -f -y
-apt-get update --allow-releaseinfo-change
-apt-get upgrade -y
-apt-get dist-upgrade -y
+apt install -f -y
+apt update --allow-releaseinfo-change
+apt upgrade -y
+apt dist-upgrade -y

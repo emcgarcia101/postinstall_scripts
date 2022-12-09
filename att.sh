dff55e61e6c213e66a392d80e900e0c0fd58ab51
Emanuel Garcia <64049524+emcgarcia101@users.noreply.github.com>
2022-12-09 12:00:18 -0300

    Update att.sh

--- att.sh
+++ att.sh
@@ -1,8 +1,8 @@
 #Creata a AutoUpdate Script (Use by own risk)
 
-echo "#!/bin/bash" >> /etc/att
-echo "sudo apt update --allow-releaseinfo-change" >> /etc/att
-echo "sudo apt upgrade -y" >> /etc/att
-echo "sudo apt dist-upgrade -y" >> /etc/att
-sudo chmod +x /etc/att
-sudo ln -s /etc/att /usr/bin/att
+echo "#!/bin/bash" >> /etc/att.sh
+echo "sudo apt update --allow-releaseinfo-change" >> /etc/att.sh
+echo "sudo apt upgrade -y" >> /etc/att.sh
+echo "sudo apt dist-upgrade -y" >> /etc/att.sh
+sudo chmod +x /etc/att.sh
+sudo ln -s /etc/att.sh /usr/bin/att

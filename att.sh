d71e5f82fdcacb1ba643a17db73a1df33aff2501
Emanuel Garcia <64049524+emcgarcia101@users.noreply.github.com>
2023-09-28 23:37:11 -0300

    Update att.sh

--- att.sh
+++ att.sh
@@ -1,8 +1,14 @@
-#Creata a AutoUpdate Script (Use by own risk)
+sudo mkdir /etc/custom_scripts/
 
-echo "#!/bin/bash" >> /etc/att.sh
-echo "sudo apt update --allow-releaseinfo-change" >> /etc/att.sh
-echo "sudo apt upgrade -y" >> /etc/att.sh
-echo "sudo apt dist-upgrade -y" >> /etc/att.sh
-sudo chmod +x /etc/att.sh
-sudo ln -s /etc/att.sh /usr/bin/att
+cat <<EOF >> /etc/custom_scripts/att.sh
+#!/bin/bash
+
+#Create a AutoUpdate Script (Use by own risk)
+
+sudo apt update --allow-releaseinfo-change
+sudo apt full-upgrade -y
+
+EOF
+
+chmod +x /etc/custom_scripts/att.sh
+ln -s /etc/custom_scripts/att.sh /usr/bin/att

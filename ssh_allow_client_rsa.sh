ac12a79154be9b87adaae59f666bddcd4247032b
Emanuel Garcia <64049524+emcgarcia101@users.noreply.github.com>
2024-03-31 14:50:32 -0300

    Update ssh_allow_client_rsa.sh

--- ssh_allow_client_rsa.sh
+++ ssh_allow_client_rsa.sh
@@ -1,6 +1,6 @@
 #Allow old RSA Algorithm via SSH
 
-sudo cat <<EOF >> /etc/ssh/sshd_config.d/allow_root.conf
+sudo cat <<EOF >> /etc/ssh/ssh_config.d/allow_client_rsa.conf
 #!/bin/bash
 
 #Allow old RSA Algorithm via SSH
